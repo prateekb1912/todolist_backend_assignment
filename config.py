92d1a4176f88fe0fcb6185065c8b9613980a91bc
prateekb1912 <prateekb1912@gmail.com>
2023-02-13 22:07:46 +0530

    Initialize project and add configuration

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+TESTING = True
+DEBUG = True
+FLASK_ENV = 'development'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'very-secret-key')
