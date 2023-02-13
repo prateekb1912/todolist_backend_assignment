46c0e0e317e77a0962c80ffe631b16dc6cb3ac4d
prateekb1912 <prateekb1912@gmail.com>
2023-02-13 23:39:57 +0530

    Minor structural changes

--- app.py
+++ app.py
@@ -5,7 +5,3 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     return "Heelo WOOOR"
-
-
-if __name__ == '__main__':
-    app.run('localhost', 5000)
--- main/config.py
+++ main/config.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(BASE_DIR, '.env'))
 
-TESTING = True
-DEBUG = True
-FLASK_ENV = 'development'
-SECRET_KEY = os.environ.get('SECRET_KEY', 'very-secret-key')
+class Config:
+    DEBUG = True
+    FLASK_ENV = 'development'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'very-secret-key')
+
